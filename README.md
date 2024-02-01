bb9c822b507a80312d518c7dfbb157806f673432
melekpro <melekpro6@gmail.com>
2024-02-01 19:40:31 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# twrp_itel-A662L
+# Android device tree for Itel itel A662L (itel-A662L)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_itel-A662L-user
+add_lunch_combo omni_itel-A662L-userdebug
+add_lunch_combo omni_itel-A662L-eng
